26f9ded5a7c7da64a7f839ba31f2752de3400760
silvana <silvana@silvanaHP>
2013-03-18 14:01:13 -0300

    agregue stfy.py

---
+++ prueba/src/prueba.py
@@ -0,0 +1,8 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="silvana"
+__date__ ="$18/03/2013 01:59:22 PM$"
+
+if __name__ == "__main__":
+    print "Hello World"
---
+++ prueba/src/stfy.py
@@ -0,0 +1,8 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="silvana"
+__date__ ="$18/03/2013 02:00:00 PM$"
+
+if __name__ == "__main__":
+    print "Hello World"
